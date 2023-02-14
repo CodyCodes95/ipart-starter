130eaa6b32c5b52e43d0d5263c579c2c4822b2ff
Cody Partington <codythatsme@gmail.com>
2023-02-14 11:13:10 +1100

    not rendering settings component

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import Settings from "./Settings";
 
 if (document.getElementById("JsonSettings")) {
-    ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    );
+  ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+      <App />
+      <Settings />
+    </React.StrictMode>
+  );
 } else {
   ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
@@ -16,4 +18,3 @@ if (document.getElementById("JsonSettings")) {
     </React.StrictMode>
   );
 }
-
