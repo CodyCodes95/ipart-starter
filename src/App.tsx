9b39e33f460b8d1dbf9b3899cbf1dc339534725c
Cody Partington <codythatsme@gmail.com>
2023-02-28 14:41:31 +1100

    added loader while loading ipart settings

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import { useEffect, useState } from "react";
 import { getIpartSettings } from "./utils/imisFetchOptions";
 import toast, { Toaster } from "react-hot-toast";
 import { showError, showSuccess } from "./utils/toast";
+import loader from "./assets/loader.svg";
 
 const App = () => {
-
   const [exampleValue, setExampleValue] = useState<string>("");
   const [sampleIqaPath, setSampleIqaPath] = useState<string>("");
-
+  const [settingsRetrieved, setSettingsRetrieved] = useState<boolean>(false);
 
   const setSettings = async () => {
-    const {exampleValue, sampleIqaPath} = await getIpartSettings()
+    const { exampleValue, sampleIqaPath } = await getIpartSettings();
     setExampleValue(exampleValue);
     setSampleIqaPath(sampleIqaPath);
+    setSettingsRetrieved(true);
   };
 
   useEffect(() => {
@@ -20,14 +21,20 @@ const App = () => {
     setSettings();
   }, []);
 
+  if (!settingsRetrieved) {
+    <div className="flex min-h-screen w-full flex-col items-center justify-center">
+      <img className="h-[15rem] w-[15rem]" src={loader} />
+    </div>;
+  }
+
   return (
-      <Toaster
-        position="top-center"
-        containerStyle={{
-          zIndex: 10000000,
-          top: 250,
-        }}
-      />
+    <Toaster
+      position="top-center"
+      containerStyle={{
+        zIndex: 10000000,
+        top: 250,
+      }}
+    />
   );
 };
 
