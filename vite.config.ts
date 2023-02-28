d9fb71f7c545432d800025679e483317baac6ad2
Cody Partington <codythatsme@gmail.com>
2023-02-28 16:06:59 +1100

    better default path for azure

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 const noAttr = () => {
   return {
     name: "no-attribute",
-    transformIndexHtml(html:any) {
+    transformIndexHtml(html: any) {
       return html.replace(`type="module" crossorigin`, "defer");
     },
   };
@@ -12,5 +12,5 @@ const noAttr = () => {
 
 export default defineConfig({
   plugins: [react(), noAttr()],
-  base: "https://cloud.causeis.com.au/iParts/PATHHERE",
-})
+  base: "https://smartsuite.blob.core.windows.net/iparts/IPARTNAME/",
+});
