dc4f31e1173a45be5cc7f0dd0ed00f1923faa641
Cody Partington <codythatsme@gmail.com>
2023-05-02 22:49:17 +1000

    react query example added

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { useEffect, useState } from "react";
 import { getIpartSettings } from "./utils/imisUtils";
 import toast, { Toaster } from "react-hot-toast";
 import loader from "./assets/loader.svg";
+import { imisFetch } from "./utils/fetches";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 
 const App = () => {
   const [exampleValue, setExampleValue] = useState<string>("");
   const [sampleIqaPath, setSampleIqaPath] = useState<string>("");
   const [settingsRetrieved, setSettingsRetrieved] = useState<boolean>(false);
+  const queryClient = useQueryClient();
 
   const setSettings = async () => {
     // const { exampleValue, sampleIqaPath } = await getIpartSettings();
@@ -20,6 +23,17 @@ const App = () => {
     setSettings();
   }, []);
 
+  const getData = async () => {
+    const res = await imisFetch("/api/party?limit=3", "GET");
+    return res;
+  };
+
+  const party = useQuery({ queryKey: ["party"], queryFn: getData });
+
+  useEffect(() => {
+    console.log(party);
+  }, [party]);
+
   if (!settingsRetrieved) {
     <div className="flex min-h-screen w-full flex-col items-center justify-center">
       <img className="h-[15rem] w-[15rem]" src={loader} />
@@ -28,12 +42,12 @@ const App = () => {
 
   return (
     <div className="flex w-full flex-col">
-    <Toaster
-      position="bottom-right"
-      containerStyle={{
-        zIndex: 10000000,
-        top: 250,
-      }}
+      <Toaster
+        position="bottom-right"
+        containerStyle={{
+          zIndex: 10000000,
+          top: 250,
+        }}
       />
       <h1>Test</h1>
     </div>
