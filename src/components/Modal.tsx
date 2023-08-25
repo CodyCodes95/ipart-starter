05226400a85ea515206bf71b7fac6ae67abc25c9
Cody Partington <cody@causeis.com.au>
2023-08-25 16:46:01 +1000

    modal updated

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -26,29 +26,32 @@ const Modal: FC<ModalProps> = ({
       className="relative z-[5001]"
     >
       {/* The backdrop, rendered as a fixed sibling to the panel container */}
-      <div
-        className="fixed inset-0 bg-black/30 backdrop-blur"
-        aria-hidden="true"
-      />
+      <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
 
       {/* Full-screen container to center the panel */}
-      <div className="fixed inset-0 flex items-center justify-center p-4">
+      <div className="fixed inset-0 flex flex-col items-center justify-center p-4">
         {/* The actual dialog panel  */}
+        <div
+          className={`flex items-center justify-between rounded-t-lg border-b-2 bg-gray-100 p-4 ${
+            className?.includes("w-")
+              ? `${className.split(" ").filter((c) => c.includes("w-"))[0]}`
+              : "w-1/2"
+          }  
+      }`}
+        >
+          <Dialog.Title className="text-3xl font-medium">{title}</Dialog.Title>
+          <X
+            className="h-12 w-12 cursor-pointer rounded-md p-2 hover:bg-gray-200"
+            onClick={onClose}
+          />
+        </div>
         <Dialog.Panel
+          id="modal-panel"
           className={cn(
-            "better-scrollbar mx-auto overflow-y-auto rounded-md bg-white",
+            "mx-auto overflow-y-auto rounded-b-md bg-white shadow-2xl",
             className
           )}
         >
-          <div className="flex w-full items-center justify-between rounded-t-lg border-b-2 p-4">
-            <Dialog.Title className="text-3xl font-medium">
-              {title}
-            </Dialog.Title>
-            <X
-              className="h-12 w-12 cursor-pointer rounded-md p-2 hover:bg-gray-200"
-              onClick={onClose}
-            />
-          </div>
           {children}
         </Dialog.Panel>
       </div>
