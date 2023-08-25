b2e841c825101f960a26d3ec83e66fd7ea32b117
Cody Partington <cody@causeis.com.au>
2023-08-25 16:57:59 +1000

    backdrop blur added

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -26,7 +26,10 @@ const Modal: FC<ModalProps> = ({
       className="relative z-[5001]"
     >
       {/* The backdrop, rendered as a fixed sibling to the panel container */}
-      <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+      <div
+        className="fixed inset-0 bg-black/30 backdrop-blur"
+        aria-hidden="true"
+      />
 
       {/* Full-screen container to center the panel */}
       <div className="fixed inset-0 flex flex-col items-center justify-center p-4">
