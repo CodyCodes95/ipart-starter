93bd0bcdbf45e955e150786c4d25d96a3c1feadd
Cody Partington <cody@causeis.com.au>
2023-09-10 13:27:17 +1000

    sonner added main simplified

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./index.css";
 import Settings from "./Settings";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "react-hot-toast";
+import { Toaster } from "sonner";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -13,27 +13,11 @@ const queryClient = new QueryClient({
   },
 });
 
-if (document.getElementById("JsonSettings")) {
-  ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <Settings />
-      </QueryClientProvider>
-    </React.StrictMode>
-  );
-} else {
-  ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <App />
-        <Toaster
-          position="bottom-right"
-          containerStyle={{
-            zIndex: 10000000,
-            top: 250,
-          }}
-        />
-      </QueryClientProvider>
-    </React.StrictMode>
-  );
-}
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      {document.querySelector("#JsonSettings") ? <Settings /> : <App />}
+      <Toaster position="bottom-right" richColors />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
